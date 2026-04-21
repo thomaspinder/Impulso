a30965b70b08cb490347f59109e27a9dbc1ce071
Thomas Pinder <tompinder@live.co.uk>
2026-04-21 11:50:43 +0200

    test(sv): cover all SV names in public-API import tests

--- tests/test_sv_public_api.py
+++ tests/test_sv_public_api.py
@@ -2,17 +2,34 @@
 
 
 def test_top_level_imports():
-    from impulso import FittedSV, StochasticVolatility, SVData
-
-    assert SVData is not None
-    assert StochasticVolatility is not None
-    assert FittedSV is not None
-
-
-def test_subpackage_imports():
-    from impulso.sv import FittedSV, StochasticVolatility, SVData, SVDefaultPrior
+    from impulso import (
+        FittedSV,
+        StochasticVolatility,
+        SVData,
+        SVDefaultPrior,
+        SVForecastResult,
+        VolatilityResult,
+    )
 
     assert SVData is not None
     assert StochasticVolatility is not None
     assert FittedSV is not None
     assert SVDefaultPrior is not None
+    assert VolatilityResult is not None
+    assert SVForecastResult is not None
+
+
+def test_subpackage_imports():
+    from impulso.sv import (
+        FittedSV,
+        StochasticVolatility,
+        SVData,
+        SVDefaultPrior,
+        SVPrior,
+    )
+
+    assert SVData is not None
+    assert StochasticVolatility is not None
+    assert FittedSV is not None
+    assert SVDefaultPrior is not None
+    assert SVPrior is not None
